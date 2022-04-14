47069daab3fdd05ea9dccf607d0f95557c4b5102
Utkarsh Saxena <61586532+UtkarshSaxena2001@users.noreply.github.com>
2022-04-14 18:31:47 +0530

    VITFED029

---
+++ app.js
@@ -0,0 +1,90 @@
+function revalImg5()
+{
+    document.getElementById("img5").hidden = false;
+    document.getElementById("num5").innerHTML = "5. The Book of Boba Fett";
+    document.getElementById("pg5").hidden = false;
+}
+function revalImg4()
+{
+    document.getElementById("img4").hidden = false;
+    document.getElementById("num4").innerHTML = "4. The Mandalorian";
+    document.getElementById("pg4").hidden = false;
+}
+function revalImg3()
+{
+    document.getElementById("img3").hidden = false;
+    document.getElementById("num3").innerHTML = "3. WandaVision";
+    document.getElementById("pg3").hidden = false;
+}
+function revalImg2()
+{
+    document.getElementById("img2").hidden = false;
+    document.getElementById("num2").innerHTML = "2. Loki";
+    document.getElementById("pg2").hidden = false;
+}
+function revalImg1()
+{
+    document.getElementById("img1").hidden = false;
+    document.getElementById("num1").innerHTML = "1. Obi-Wan Kenobi";
+    document.getElementById("pg1").hidden = false;
+}
+function revalImgMov5()
+{
+    document.getElementById("imgMov5").hidden = false;
+    document.getElementById("numMov5").innerHTML = "5. Star Wars: Return of the Jedi (Episode VI)";
+    document.getElementById("pgMov5").hidden = false;
+}
+function revalImgMov4()
+{
+    document.getElementById("imgMov4").hidden = false;
+    document.getElementById("numMov4").innerHTML = "4. Black Panther";
+    document.getElementById("pgMov4").hidden = false;
+}
+function revalImgMov3()
+{
+    document.getElementById("imgMov3").hidden = false;
+    document.getElementById("numMov3").innerHTML = "3. SpiderMan Homecoming";
+    document.getElementById("pgMov3").hidden = false;
+}
+function revalImgMov2()
+{
+    document.getElementById("imgMov2").hidden = false;
+    document.getElementById("numMov2").innerHTML = "2. demon slayer mugen train";
+    document.getElementById("pgMov2").hidden = false;
+}
+function revalImgMov1()
+{
+    document.getElementById("imgMov1").hidden = false;
+    document.getElementById("numMov1").innerHTML = "1. SpiderMan No Way Home";
+    document.getElementById("pgMov1").hidden = false;
+}
+function revalImgAn5()
+{
+    document.getElementById("imgAn5").hidden = false;
+    document.getElementById("numAn5").innerHTML = "5. Bleach";
+    document.getElementById("pgAn5").hidden = false;
+}
+function revalImgAn4()
+{
+    document.getElementById("imgAn4").hidden = false;
+    document.getElementById("numAn4").innerHTML = "4. Naruto Shipuden";
+    document.getElementById("pgAn4").hidden = false;
+}
+function revalImgAn3()
+{
+    document.getElementById("imgAn3").hidden = false;
+    document.getElementById("numAn3").innerHTML = "3. Death Note";
+    document.getElementById("pgAn3").hidden = false;
+}
+function revalImgAn2()
+{
+    document.getElementById("imgAn2").hidden = false;
+    document.getElementById("numAn2").innerHTML = "2. One punch Man";
+    document.getElementById("pgAn2").hidden = false;
+}
+function revalImgAn1()
+{
+    document.getElementById("imgAn1").hidden = false;
+    document.getElementById("numAn1").innerHTML = "1. Attack On Titan";
+    document.getElementById("pgAn1").hidden = false;
+}
